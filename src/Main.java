bf9c59cebfcec007e8188f2d710d7e216e3221aa
qmargo23 <elen.asplund@gmail.com>
2023-11-30 14:18:15 +0300

    release_from_dev

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("7.1_командная работа в Gitflow");
         System.out.println("создали ветку dev    (DEVELOPER) от master");
-        System.out.println("создали ветку feature_demo_1    (FEATURE) от dev");
+
+        System.out.println("создали ветку release    (RELEASE) от dev");
 
     }
 }
